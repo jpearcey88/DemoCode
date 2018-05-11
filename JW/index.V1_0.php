edd7b6271e813e2e367652c1a86da7e1f9f95d9d
Joseph Pearcey <jpearcey@msn.com>
2018-05-11 16:02:54 +1000

    Organising
    added and organising Demo Code.

---
+++ JW/index.V1_0.php
@@ -0,0 +1,123 @@
+<?php
+$table1 = [
+    [
+        "id" => 1,
+        "date" => '2012-01-01',
+        "engine" => "google",
+        "searchTerm" => "widgets",
+        "ranking" => 7
+    ], [
+        "id" => 2,
+        "date" => '2012-01-01',
+        "engine" => "yahoo",
+        "searchTerm" => "widgets",
+        "ranking" => 6
+    ], [
+        "id" => 3,
+        "date" => '2012-01-01',
+        "engine" => "bing",
+        "searchTerm" => "widgets",
+        "ranking" => 8
+    ], [
+        "id" => 4,
+        "date" => '2012-01-01',
+        "engine" => "google",
+        "searchTerm" => "green widgets",
+        "ranking" => 20
+    ], [
+        "id" => 5,
+        "date" => '2012-01-01',
+        "engine" => "yahoo",
+        "searchTerm" => "green widgets",
+        "ranking" => 0
+    ], [
+        "id" => 6,
+        "date" => '2012-01-01',
+        "engine" => "bing",
+        "searchTerm" => "green widgets",
+        "ranking" => 29
+    ]
+];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table, tr {
+                padding: 0;
+                width: 100%;
+                border-bottom:1px solid black;
+            }
+            td {
+                padding: 5px;
+                border-top:1px solid black;
+                border-left:1px solid black;
+            }
+            td:last-child{
+                border-right:1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <h3>table 1</h3>
+        <table cellpadding="0" cellspacing="0">
+            <tr><td>id</td><td>date</td><td>engine</td><td>searchTerm</td><td>ranking</td></tr>
+            <?php
+            foreach ($table1 as $r) {
+                echo sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%d</td></tr>', $r["id"], $r["date"], $r["engine"], $r["searchTerm"], $r["ranking"]);
+            }
+            ?>
+        </table>
+        <h3>table 2</h3>
+        <table cellpadding="0" cellspacing="0">
+            <tr><td>id</td><td>date</td><td>searchTerm</td><td>visibility</td></tr>
+            <?php
+            $startTime = microtime(true);
+            for ($i = 0; $i < 1000000; $i++) {
+                $table2Data = table2Data($table1);
+            }
+            echo "Time: " . number_format(( microtime(true) - $startTime), 4) . " Seconds\n";
+            foreach ($table2Data as $r) {
+                echo sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%01.1f</td></tr>', $r["id"], $r["date"], $r["searchTerm"], $r["visibility"]);
+            }
+            ?>
+        </table>
+    </body>
+</html>
+<?php
+
+function table2Data(&$table1) {
+    $proccessData = [];
+    $searchTermRankings = [];
+    foreach ($table1 as $row) {
+        $searchTermRankings[$row["date"]][$row["searchTerm"]][$row["engine"]] = $row["ranking"];
+    }
+    foreach ($searchTermRankings as $d => $searchterms) {
+        foreach ($searchterms as $searchterm => $ranks) {
+            $proccessData[] = [
+                "id" => sizeof($proccessData) + 1,
+                "date" => $d,
+                "searchTerm" => $searchterm,
+                "visibility" => calculateVisibility($ranks)
+            ];
+        }
+    }
+    return $proccessData;
+}
+
+function calculateVisibility(Array &$ranks) {
+    $a = calcRank(getArrayValue("google", $ranks)) * 17;
+    $b = calcRank(getArrayValue("yahoo", $ranks)) * 2;
+    $c = calcRank(getArrayValue("bing", $ranks));
+    return ($a + $b + $c / 600) * 100;
+}
+
+function calcRank(int $rank) {
+    return ($rank > 0 ? 31 - $rank : 0);
+}
+
+function getArrayValue(string $key, &$arr) {
+    return (array_key_exists($key, $arr) ? $arr[$key] : 0);
+}
---
+++ JW/readme.md
@@ -0,0 +1,4 @@
+Version 1 lacks the ability to be easily updated with sums.
+Version 1.5 is now more easily updated.
+Version 2 is done as a class with a constant for search engine types.
+Version 2.5 has less checks than version 2 and all functions are static.
